9c3da75cd017248b068b43e4ff85b1a295a80cda
yunbiyomi <yunbiyomi@naver.com>
2023-08-11 00:23:58 +0900

    🐛 Fix : APIKEY 환경 변수로 설정

--- api/movie.js
+++ api/movie.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
 
+const { APIKEY } = process.env
+
 export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body)
   const url = id 
-    ? `https://omdbapi.com?apikey=7f68ae33&i=${id}&plot=full` 
-    : `https://omdbapi.com?apikey=7f68ae33&s=${title}&page=${page}`
+    ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full` 
+    : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
   const res = await fetch(url)
   const json = await res.json()
   response
